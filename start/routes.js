990bf5b6a831abdc7b67f4eaee047ee78144f058
Ryker Southwick <rykersouthwick@gmail.com>
2019-11-05 15:21:03 -0700

    add auth routes

--- start/routes.js
+++ start/routes.js
@@ -17,10 +17,44 @@ const Route = use('Route')
 
 const User = use('App/Models/User')
 
+// Generic Social Authentiction Routes
+// Can be used with Twitter, Google, Facebook, Instagram, etc..
+
+Route.get('login/???', async ({
+  ally
+}) => {
+  await ally.driver('???').redirect()
+})
+
+Route.get('/authenticated/???', async ({
+  ally,
+  response,
+  auth
+}) => {
+  const allyUser = await ally.driver('???').getUser()
+
+  // user details to be saved
+  const userDetails = {
+    nickname: allyUser.getNickname(),
+    email: allyUser.getEmail(),
+    avatar: allyUser.getAvatar(),
+    login_source: '???',
+  }
+
+  // search for existing user
+  const whereClause = {
+    email: allyUser.getEmail()
+  }
+
+  const user = await User.findOrCreate(whereClause, userDetails)
+  await auth.login(user)
+
+  return 'test'
+})
+
+
 Route.get('/', async ({
   view
 }) => {
-  const users = await User.all(); 
-  console.log(users.toJSON()[0])
   return view.render('index')
 })
